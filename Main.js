1b416aa0bc1ec99db650cc39acf4fd937234192e
Shawn <shawn.m.dietrich@gmail.com>
2022-02-07 16:27:07 -0500

    Testing out classes and the export / import

--- Main.js
+++ Main.js
@@ -1,2 +1,11 @@
 //Main javascript file will interact with the HTML file
 //All objects should be initlized in the obj.js file
+//Import all exported classes and objects
+import * as objects from './obj.js';
+//import {testObject} from './obj.js';
+let objClass = new objects.messageObjects();
+objClass.print()
+
+console.log(objects.testObject.name)
+
+
---
+++ obj.js
@@ -0,0 +1,14 @@
+
+export class messageObjects {
+    constructor(){   } 
+    print(){
+            console.log("Hello World");
+        };
+}
+
+export const testObject = {
+    name : 'Joe',
+    age : 34
+};
+
+
