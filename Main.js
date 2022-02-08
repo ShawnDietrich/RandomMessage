1ac780c85e5b012a257db9733bc28ba96c207287
Shawn <shawn.m.dietrich@gmail.com>
2022-02-08 07:35:45 -0500

    Updated scope of project

--- Main.js
+++ Main.js
@@ -1,11 +1,12 @@
-//Main javascript file will interact with the HTML file
-//All objects should be initlized in the obj.js file
-//Import all exported classes and objects
-import * as objects from './obj.js';
-//import {testObject} from './obj.js';
-let objClass = new objects.messageObjects();
-objClass.print()
+//Declare all objects here
 
-console.log(objects.testObject.name)
+
+
+
+
+
+
+
+//Declare any functions required here
 
 
